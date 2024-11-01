997890952ccfd2465d3dc166593c0154e619b750
Gustavo Nunes Lopes <gustavo.lopes@faex.edu.br>
2024-10-31 22:54:20 -0300

    endpoint

---
+++ src/Controllers/Panel/Students/ApiStudents.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Gustavo\Students\Controllers\Panel\Students;
+
+use Gustavo\Students\Helpers\Template\Loader;
+use Gustavo\Students\Models\Students\Student;
+
+class ApiStudents
+{
+    private Loader $template;
+    protected Student $student;
+    
+    public function __construct() {
+
+        $this->template = new Loader();
+
+        $this->student = new Student();
+    }
+
+    public function execute(){
+
+        $student = $this->student->findAll();
+
+        echo json_encode($student);
+        
+    }
+}
